69e2a0acf2136acfc50498b3098de122d83cb2d2
Puneet Aggarwal <9988182547p@gmail.com>
2016-10-24 13:06:47 +0530

    changes in login backen

--- login_backend.php
+++ login_backend.php
@@ -1,7 +1,10 @@
 <?php
+session_start() ;
+
 include 'dbcon.php' ;
 $sid = $_POST['surveyid'] ;
 $pass = $_POST['password'] ;
+$_SESSION["surveyid"] = $sid ;
 $qry = "SELECT * from survey WHERE survey_id='".$sid."'"." AND survey_password = '".$pass."'" ;
 // echo $qry ;
 $result = $conn->query($qry);
