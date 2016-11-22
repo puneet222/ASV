c91a242cb98ff8abec36a93bdaab1de981499c94
Puneet Aggarwal <9988182547p@gmail.com>
2016-11-22 15:51:54 +0530

    submitting the response of the mcq question

---
+++ submitresponse.php
@@ -0,0 +1,64 @@
+<?php
+include("dbcon.php");
+$sid = $_POST["surveyid"] ;
+$email = $_POST["email"] ;
+$data = $_POST["str"] ;
+echo $data."--------- in php" ;
+$ques = explode(",", $data) ;
+
+for ($i=0; $i < count($ques); $i++) {
+  $arr = explode("-", $ques[$i]) ;
+  if($arr[1] == "1")
+  {
+    // type 1 mcq questions
+    $query = "SELECT * FROM `survey_answer_type1` WHERE qid = "."'". $arr[0] . "'" ;
+    $result = $conn->query($query);
+    while($row = $result->fetch_assoc()) {
+        $ans = $row["response"] ;
+        $opt = explode(",", $ans);
+        $temp = $opt[$arr[2]] ;
+        $temp++ ;
+        $opt[$arr[2]] = $temp;
+        $opt = implode(",", $opt) ;
+        echo $opt ;
+        echo "\n";
+        $query = "UPDATE `survey_answer_type1` SET `response`=". "'" . $opt . "'" ." WHERE qid = "."'". $arr[0] . "'" ;
+        mysqli_query($conn , $query) ;
+    }
+  }
+  else if($arr[1] == "3")
+  {
+    // type 3 questions
+    $query = "SELECT * FROM `survey_answer_type3` WHERE qid = "."'". $arr[0] . "'" ;
+    $result = $conn->query($query);
+    while($row = $result->fetch_assoc()) {
+        $ans = $row["response"] ;
+        $opt = explode(",", $ans);
+        $temp = $opt[$arr[2]] ;
+        $temp++ ;
+        $opt[$arr[2]] = $temp;
+        $opt = implode(",", $opt) ;
+        echo $opt ;
+        echo "\n";
+        $query = "UPDATE `survey_answer_type3` SET `response`=". "'" . $opt . "'" ." WHERE qid = "."'". $arr[0] . "'" ;
+        mysqli_query($conn , $query) ;
+    }
+  }
+  else if($arr[1] == "4"){
+    $query = "SELECT * FROM `survey_answer_type4` WHERE qid = "."'". $arr[0] . "'" ;
+    $result = $conn->query($query);
+    while($row = $result->fetch_assoc()) {
+        $ans = $row["response"] ;
+        $opt = explode(",", $ans);
+        $temp = $opt[$arr[2]] ;
+        $temp++ ;
+        $opt[$arr[2]] = $temp;
+        $opt = implode(",", $opt) ;
+        echo $opt ;
+        echo "\n";
+        $query = "UPDATE `survey_answer_type4` SET `response`=". "'" . $opt . "'" ." WHERE qid = "."'". $arr[0] . "'" ;
+        mysqli_query($conn , $query) ;
+    }
+  }
+}
+?>
