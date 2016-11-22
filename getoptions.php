9b2cbe707be98b17c19c7f5e006088d5922c15f8
Puneet Aggarwal <9988182547p@gmail.com>
2016-11-22 15:22:20 +0530

    initializing the table of mcq question response

--- getoptions.php
+++ getoptions.php
@@ -1,5 +1,6 @@
 <?php
-$sid = $_POST["surveyid"] ;
+// $sid = $_POST["surveyid"] ;
+$sid = $_GET["sur"] ;
 include('dbcon.php');
 $query = "SELECT * FROM `survey_answer_custom` WHERE survey_id = " . "'" . $sid . "'" ;
 // echo $query ;
@@ -11,9 +12,32 @@ $options = array() ;
 
 while($row = $result->fetch_assoc()) {
   $options[] = $row ;
+  $qid = $row["qid"] ;
+  $ans = $row["answer_option"] ;
+  $opt = explode(",", $ans) ;
+  $len = count($opt) ;
+  $str = "" ;
+  for ($i=0; $i < $len-1; $i++) {
+    $str = $str."0," ;
+  }
+  $str = $str."0" ;
+
+  // now we have the question id and survey id and intializing string
+
+  $query = "INSERT INTO `survey_answer_type1`(`qid`, `survey_id`, `response`) VALUES (?,?,?)" ;
+  $stmt = $conn->prepare($query) ;
+  $stmt->bind_param('sss', $qid , $sid , $str);
+
+  $stmt->execute() ;
+
 }
 // $return['options'] = $options ;
+echo "-------------------------------\n" ;
 echo json_encode($options) ;
+
+
+// -----------------------  INITIALIZING THE QUESTION TABLE --------------------------------
+// echo $options['qid'];
 
 // echo $opt[0] ;
 ?>
