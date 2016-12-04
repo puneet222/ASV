55893f5f81e358cddf84286740988045e20711d8
Puneet Aggarwal <9988182547p@gmail.com>
2016-12-05 02:24:31 +0530

    get options object with questions too

--- previewquestion.php
+++ previewquestion.php
@@ -11,5 +11,17 @@
   {
   $rows[]=$r;
   }
+
+  $query = "SELECT * FROM `survey_answer_custom` WHERE survey_id = "."'".$sid."'" ;
+  $res = $conn->query($query) ;
+  // $row = $res->fetch_assoc() ;
+  $rows2 = array();
+  while($r=$res->fetch_assoc())
+  {
+  $rows2[]=$r;
+  }
+
   echo json_encode($rows);
+  echo "-{]&%this_is_split$#%[{-" ;
+  echo json_encode($rows2) ;
  ?>
