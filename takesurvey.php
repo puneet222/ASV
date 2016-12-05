3f29b1840cf24ed8180a5b9b554f15217f0e915b
Puneet Aggarwal <9988182547p@gmail.com>
2016-12-06 02:51:38 +0530

    some minor changes

--- takesurvey.php
+++ takesurvey.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
-	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
     <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
     <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
@@ -11,155 +11,182 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
 
   <!-- Compiled and minified JavaScript -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="a.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
   <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
   <script src="js/materialize.js"></script>
   <script src="js/init.js"></script>
 
-	<script type="text/javascript">
-		$("#upld").hide(10) ;
-		$(".jupload").hide() ;
-		$("#ifsignup").hide() ;
-		$("#login-div").hide() ;
-		$("#incorrect").hide() ;
-		console.log("javascript") ;
-		$('document').ready(function(){
-			$("#incorrect").hide() ;
-			console.log("writing javascript") ;
-			$("#upld").hide() ;
-			$("#ifsignup").hide() ;
+  <script type="text/javascript">
+    $("#upld").hide(10) ;
+    $(".jupload").hide() ;
+    $("#ifsignup").hide() ;
+    $("#login-div").hide() ;
+    $("#incorrect").hide() ;
+    console.log("javascript") ;
+    $('document').ready(function(){
 
-			$("#open").click(function(){
-				$("#upld").fadeOut() ;
-			})
+      $("#incorrect").hide() ;
+      console.log("writing javascript") ;
+      $("#upld").hide() ;
+      $("#ifsignup").hide() ;
 
-			$("#auth").click(function(){
-				$("#upld").slideDown() ;
-			})
+      $("#open").click(function(){
+        $("#upld").fadeOut() ;
+      })
 
-			$("#lsign").click(function(){
-				// alert("click") ;
-				$("#login-div").hide("slow" , function(){
-					$("#ifsignup").show("slow") ;
-				}) ;
-			})
+      $("#auth").click(function(){
+          $("#excel").openModal();
+          $("#upld").slideDown() ;
+        })
+      $("#modalclose").click(function(){
+        $("#excel").closeModal();
 
-			$("#ins-log").click(function(){
-				// alert("vbjvbr") ;
-				$("#ifsignup").hide("slow" , function(){
-					$("#login-div").show("slow") ;
-				}) ;
-			})
+      });
 
-			$("#lsubmit").click(function(){
-				var survey_id = $("#sid").val() ;
-				var password = $("#spass").val() ;
-				var dataString = 'surveyid=' + survey_id + "&password=" + password ;
-				if(survey_id == "" || password == "")
-				{
-					alert("Please Complete the Login field") ;
-				}
-				$.ajax({
-				type: "POST",
-				url: "login_backend.php",
-				data: dataString,
-				cache: false,
-				success: function(result){
-					// alert(result) ;
-					var l = result.length ;
-					// alert(l) ;
-				if(l == 6)
-				{
-					//redirect to another page
-					// alert(survey_id) ;
-					// alert("correct") ;
-					var loc = "organizesurvey.php" ;
-					window.location.href = loc;
-				}
-				else
-				{
-					// alert(result) ;
-					$("#incorrect").show("slow") ;
-				}
-				}
-				});
-			})
-		});
-	</script>
+      $("#lsign").click(function(){
+        // alert("click") ;
+        $("#login-div").hide("slow" , function(){
+          $("#ifsignup").show("slow") ;
+        }) ;
+      })
 
-	<title>Take Survey</title>
+      $("#ins-log").click(function(){
+        // alert("vbjvbr") ;
+        $("#ifsignup").hide("slow" , function(){
+          $("#login-div").show("slow") ;
+        }) ;
+      })
 
-	<style type="text/css">
-		body{
-			background-color: #009688 ;
-		}
-		.signupdiv{
-			background-color: white ;
-			margin-top : 5% ;
-			margin-left : 30% ;
-			margin-right: 50% ;
-			width: 500px ;
-			/*border-style: solid;*/
-			/*border-color: black ;*/
-			border-radius: 15px ;
-			box-shadow: 5px 5px 5px #004D40;
-		}
-		/*#first_name{
-			background-color: white ;
-		}*/
-		#submit{
-			width: 100% ;
-			margin-left: 0px ;
-			margin-right: 15px ;
-			margin-bottom: 15px ;
-			margin-right: 15px ;
-			background-color: #FF9800 ;
-		}
-		#sinfo{
-			font-family: 'PT Sans', sans-serif;
-		}
-		#info-text{
-			color : white;
-		}
-		#loginval{
-			background-color: white ;
-			margin-top : 5% ;
-			margin-left : 30% ;
-			margin-right: 50% ;
-			width: 500px ;
-			/*border-style: solid;*/
-			/*border-color: black ;*/
-			border-radius: 15px ;
-			box-shadow: 5px 5px 5px #004D40;
+      $("#lsubmit").click(function(){
+        var survey_id = $("#sid").val() ;
+        var password = $("#spass").val() ;
+        var dataString = 'surveyid=' + survey_id + "&password=" + password ;
+        if(survey_id == "" || password == "")
+        {
+          alert("Please Complete the Login field") ;
+        }
+        $.ajax({
+        type: "POST",
+        url: "login_backend.php",
+        data: dataString,
+        cache: false,
+        success: function(result){
+          // alert(result) ;
+          var l = result.length ;
+          // alert(l) ;
+        if(l == 6)
+        {
+          //redirect to another page
+          // alert(survey_id) ;
+          // alert("correct") ;
+          var loc = "organizesurvey.php?surveyid="+ survey_id ;
+          window.location.href = loc;
+        }
+        else
+        {
+          // alert(result) ;
+          $("#incorrect").show("slow") ;
+        }
+        }
+        });
+      })
+    });
+  </script>
 
-		}
-		#lsubmit{
-			width: 100% ;
-			margin-left: 5px ;
-			margin-right: 5px ;
-			background-color: #01579B ;
-		}
-		#lsign{
-			width: 100% ;
-			margin-left: 5px ;
-			margin-right: 5px ;
-			background-color: #FF9800 ;
-		}
+  <title>Take Survey</title>
 
-		#ins-log{
-			background-color: #BF360C;
-			margin-left : 30% ;
-			margin-right: 50% ;
-			color: white ;
-		}
+  <style type="text/css">
+    body{
+      background-color: #009688 ;
+    }
+    .signupdiv{
+      background-color: white ;
+      margin-top : 5% ;
+      margin-left : 30% ;
+      margin-right: 50% ;
+      width: 500px ;
+      /*border-style: solid;*/
+      /*border-color: black ;*/
+      border-radius: 15px ;
+      box-shadow: 5px 5px 5px #004D40;
+    }
+    /*#first_name{
+      background-color: white ;
+    }*/
+    #submit{
+      width: 100% ;
+      margin-left: 0px ;
+      margin-right: 15px ;
+      margin-bottom: 15px ;
+      margin-right: 15px ;
+      background-color: #FF9800 ;
+    }
+    #sinfo{
+      font-family: 'PT Sans', sans-serif;
+    }
+    #info-text{
+      color : white;
+    }
+    #loginval{
+      background-color: white ;
+      margin-top : 5% ;
+      margin-left : 30% ;
+      margin-right: 50% ;
+      width: 500px ;
+      /*border-style: solid;*/
+      /*border-color: black ;*/
+      border-radius: 15px ;
+      box-shadow: 5px 5px 5px #004D40;
 
-	</style>
+    }
+    #lsubmit{
+      width: 100% ;
+      margin-left: 5px ;
+      margin-right: 5px ;
+      background-color: #01579B ;
+    }
+    #lsign{
+      width: 100% ;
+      margin-left: 5px ;
+      margin-right: 5px ;
+      background-color: #FF9800 ;
+    }
+
+    #ins-log{
+      background-color: #BF360C;
+      margin-left : 30% ;
+      margin-right: 50% ;
+      color: white ;
+    }
+
+  </style>
 
 
 
 </head>
 <body>
+<!-- -----------------------------MODAL FOR EXCEL----------------------------------- -->
+<div class = "modal" id = "excel">
+
+  <div class = "modal-content" id = "econtent">
+    <div id = "modal_head">
+      <div id = "modal_head_1"><center><h5>
+        Instructions for Authorized survey
+      </h5></center></div>
+    </div><!-- modal head -->
+ <center>For conducting authorized survey you need to upload an excel file which should have 3 Columns :
+ </center>
+ <BR>
+ <ul>
+   <li>First Column should have First Name
+  <li>Second Column should have Last Name
+  <li>Third Column should have Email Addresses
+ </ul>
+
+  <button id = "modalclose" class = "btn">OK</button>
+  </div><!-- modal content-->
+</div><!--modal = excel-->
 <nav class="white" role="navigation">
     <div class="nav-wrapper container">
       <a id="logo-container" href="#" class="brand-logo" style="color:black">Logo</a>
@@ -180,7 +207,7 @@
 
 <div id="login-div">
 <div id="sinfo" class="center">
-	<h2 id="info-text">Please login</h2>
+  <h2 id="info-text">Please login</h2>
 </div>
 <div id="loginval">
 <div class="row">
@@ -188,21 +215,21 @@
       <div class="row">
       <div id="incorrect" class="center">
       <h6 style="color:red">Incorrect Survey ID or Password</h6>
-      </div>   													<!--   SURVEY ID   -->
+      </div>                            <!--   SURVEY ID   -->
         <div class="input-field col s12">
           <i class="material-icons prefix">account_circle</i>
           <input id="sid" type="text" class="validate">
           <label for="sid">Survey Id</label>
         </div>
-        <div class="input-field col s12">             						 <!--    PASSWORD     -->
+        <div class="input-field col s12">                          <!--    PASSWORD     -->
           <i class="material-icons prefix">lock</i>
           <input id="spass" type="password" class="validate" name="password">
           <label for="spass">Password</label>
         </div>
-        <div class="input-field col s6">             						 <!--    Login BUTTON     -->
+        <div class="input-field col s6">                         <!--    Login BUTTON     -->
           <a class="btn" id="lsubmit">Login</a>
         </div>
-        <div class="input-field col s6">             						 <!--   Signup BUTTON     -->
+        <div class="input-field col s6">                         <!--   Signup BUTTON     -->
           <a class="btn" id="lsign">Signup</a>
         </div>
       </div>
@@ -213,12 +240,12 @@
 
 <div id="ifsignup" >
 <div id="sinfo" class="center">
-	<h2 id="info-text">Please Signup</h2>
+  <h2 id="info-text">Please Signup</h2>
 </div>
 
 <div class="signupdiv">
 <div class="row">
-    <form class="col s12" id="signup" method="post"  action = "survey_backend.php">
+      <form class="col s12" id="signup" method="post" enctype="multipart/form-data" action = "survey_backend.php">
       <div class="row">     <!-- first row                  First name   and   Last Name-->
         <div class="input-field col s6">
           <input  id="first_name" type="text" class="validate"  name = "first_name" >
@@ -229,25 +256,25 @@
           <label for="last_name">Last Name</label>
         </div>
       </div>
-      <!-- <div class="row">				  second row
+      <!-- <div class="row">          second row
         <div class="input-field col s12">
           <input disabled value="I am not editable" id="disabled" type="text" class="validate">
           <label for="disabled">Disabled</label>
         </div>
       </div> -->
-      <div class="row">					 <!-- second row                Oragnisation   Name  -->
+      <div class="row">          <!-- second row                Oragnisation   Name  -->
         <div class="input-field col s12">
           <input id="organisation" type="text" class="validate"  name = "org_name">
           <label for="Organisation Name">Oragnisation Name</label>
         </div>
       </div>
-      <div class="row">					 <!-- third row               Password        -->
+      <div class="row">          <!-- third row               Password        -->
         <div class="input-field col s12">
           <input id="password" type="password" class="validate"   name = "password">
           <label for="password">Password</label>
         </div>
       </div>
-      <div class="row">					 <!-- forth row             Re - enter    Password     -->
+      <div class="row">          <!-- forth row             Re - enter    Password     -->
         <div class="input-field col s12">
           <input id="repassword" type="password" class="validate" >
           <label for="repassword">Re-enter Password</label>
@@ -260,48 +287,48 @@
         </div>
       </div>
 
-      <div class="row">					 <!-- fifth  row           email  id          -->
+      <div class="row">          <!-- fifth  row           email  id          -->
         <div class="input-field col s12">
           <input id="email" type="email" class="validate"  name = "email">
           <label for="email">Email</label>
         </div>
-        <div class="row">					 <!-- seventh row          radio button         -->
+        <div class="row">          <!-- seventh row          radio button         -->
         <div class="input-field col s6">
           <p>
-      		<input name="type" type="radio" id="open" value="open">
-      		<label for="open">Open</label>
-    	  </p>
+          <input name="type" type="radio" id="open" value="open">
+          <label for="open">Open</label>
+        </p>
         </div>
         <div class="input-field col s6">
           <p>
-      		<input name="type" type="radio" id="auth" value="auth">
-      		<label for="auth">Authorized</label>
-    	  </p>
+          <input name="type" type="radio" id="auth" value="auth">
+          <label for="auth">Authorized</label>
+        </p>
         </div>
 
-      <div class="row"  class="jupload">					 <!-- sixth  row          uplaod file       -->
+      <div class="row"  class="jupload">           <!-- sixth  row          uplaod file       -->
         <div class="input-field col s12" >
           <div class="file-field input-field" id="upld">
-	      <div class="btn">
-	        <span>File</span>
-	        <input type="file">
-	      	</div>
-	      	<div class="file-path-wrapper">
-	        <input class="file-path validate" type="text">
-	      	</div>
-	    	</div>
+        <div class="btn">
+          <span>File</span>
+          <input type="file" name = "a">
+          </div>
+          <div class="file-path-wrapper">
+          <input class="file-path validate" type="text">
+          </div>
+        </div>
         </div>
       </div>
        </div>
       </div>
-      <div class="row">					 <!-- sixth  row          signup button         -->
+      <div class="row">          <!-- sixth  row          signup button         -->
         <div class="input-field col s12">
           <input type="submit" value="Sign Up" class='btn' id="submit">
         </div>
       </div>
     </form>
     </div>
-	<div id="insign-login">                								<!--     SMALL    LOGIN LINK IN SIGN UP page    -->
+  <div id="insign-login">                               <!--     SMALL    LOGIN LINK IN SIGN UP page    -->
     <button id="ins-log" class="center btn">Login</button>
     </div>
   </div>
