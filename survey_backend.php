4edce559d3b268a98b86a86df7bfb08bdb8578a7
Puneet Aggarwal <9988182547p@gmail.com>
2016-12-06 02:50:30 +0530

    signup backend

--- survey_backend.php
+++ survey_backend.php
@@ -4,55 +4,69 @@ $fname =  $_POST["first_name"];
 $lname = $_POST["last_name"];
 $org = $_POST["org_name"];
 $pass = $_POST["password"];
-$email = $_POST["email"];
 $sname = $_POST["survey_heading"];
 $type = $_POST["type"];
-echo $type;
+$sid = substr($org,0,3).substr($sname,0,3);
+$email = $_POST['email'];
 if($type == "auth")
 $x = 1;
 else
 $x = 0;
-$num;
-// $sql = "select survey from ids";
-// // $stmt = $mysqli->prepare("INSERT INTO CountryLanguage VALUES (?, ?, ?, ?)");
-// $ret = mysqli_query($conn , $sql);
-// while($row = mysqli_fetch_assoc($ret)){
-// 	$num = $row['survey'];
-// 	echo $num;
-// 	}
-// 	$num = $num + 1;
-// $sid = substr($org,0,3).substr($sname,0,3).substr($type,0,2).$num;
-// $q = 'update ids set survey = '.$num;
-
+$num = 0;
 echo $fname." ".$lname." ".$org." ".$pass." ".$email." ".$sname." ".$type."<BR>";
+$query = "INSERT INTO `auto_survey_voting`.`survey` (`organisation`, `survey_id`, `survey_password`, `auth`, `survey_heading`, `first_name`, `last_name`, `email_id` , `start`) VALUES (?,?,?,?,?, ?, ?, ?,?)";
+$select = "select survey from ids";
+$result = $conn->query($select);
 
 
-$query = "insert into survey values('".$org."','".$sid."','".$pass."','".$x."','".$sname."','".$fname."','".$lname."','".$email."');";
-if($query = $mysqli->prepare("INSERT into survey VALUES (?,?,?,?,?,?,?,?)"))
+$fn;$ln;$userpass = 'password';$em;$filled = 'N';
+
+while($row = $result->fetch_assoc())
+$num = $row['survey'];
+$sid = $sid.($num+1);
+$update = 'update ids set survey = ?';
+$stt = $conn->prepare($update);
+$num = $num+1;
+$stt->bind_param('s',$num);
+$stt->execute();
+$stmt = $conn->prepare($query);
+$start = 0 ;
+$stmt->bind_param('sssissssi',$org,$sid,$pass,$x,$sname,$fname,$lname,$email,$start);
+$stmt->execute();
+$insert = "INSERT INTO `auto_survey_voting`.`auth_table` (`survey_id`, `user_pass`, `email_id`, `filled`, `first_name`, `last_name`) VALUES (?, ?, ?, ?, ?, ?);";
+if($x == 1)
 {
-	 $query->bind_param("sssissss",$fname , $lname , $org , $pass , $email , $sname  , $type );
-	 echo "string";
-	 echo $query."sfjewoifn" ;
-	if( $query->execute())
-	{
 
-	}
-	else
-	{
-		echo "insert query not executed" ;
-	}
+if (($handle = fopen($_FILES["a"]["tmp_name"], "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ","))) {
+        $numm = count($data);
+        $fn = $data[0];
+        $ln = $data[1];
+        $em = $data[2];
+    	if($aa = $conn->prepare($insert))
+    		echo "true";
+    	else
+    		echo "false";
+    	echo $fn.$ln.$em.$sid.$userpass.$filled."<BR>";
+    	$aa->bind_param('ssssss',$sid,$userpass,$em,$filled,$fn,$ln);
+    	//$aa->bind_param('ssssss',$sid,$sid,$sid,$sid,$sid,$sid);
+    	$aa->execute();
+    }
+    fclose($handle);
+}
 }
 else
-{
-	echo "query not prepared" ;
-}
+;
 
-echo $query;
-mysqli_query($conn , $q);
-// $test = mysql_query($query,$conn);
-// if(!$test)
-// {
-// 	echo "fail";
-// }
-mysql_close($conn);
+
+function Redirect($url, $permanent = false)
+{
+    if (headers_sent() === false)
+    {
+        header('Location: ' . $url, true, ($permanent === true) ? 301 : 302);
+    }
+
+    exit();
+}
+Redirect("takesurvey.php");
 ?>
